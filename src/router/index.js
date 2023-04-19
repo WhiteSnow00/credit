85be795317cb283c4626582d15fad5ae06b10764
WhiteSnow00 <kazenosakura100@gmail.com>
2023-04-19 17:49:05 +0700

    where's the issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import MainPage from '@/components/MainPage';
 
-Vue.use(Router);
+const routes = [
+  {
+    path: '/',
+    name: 'main-page',
+    component: MainPage,
+  },
+];
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'main-page',
-      component: MainPage
-    },
-  ],
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
+
+export default router;

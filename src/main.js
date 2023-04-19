ab1f1dae84b6faee73df7695ed4db896e08f4604
WhiteSnow00 <kazenosakura100@gmail.com>
2023-04-19 20:20:04 +0700

    done 1/10

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import $ from 'jquery';
 
-
-
 window.$ = $;
 window.jQuery = $;
 
-Vue.config.productionTip = false;
+const app = createApp(App);
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app');
+app.use(router);
+app.mount('#app');
